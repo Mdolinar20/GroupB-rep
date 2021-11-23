c8c7a609961172a417bc45c27860cc1094407464
Zachary Waters <S-Zachary.Waters@Lwtech.Edu>
2021-11-22 19:28:00 -0800

    zwaters_branch

--- GroupB_Project/obj/Release/net5.0/GroupB_Project.AssemblyInfo.cs
+++ GroupB_Project/obj/Release/net5.0/GroupB_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-GroupB_Project-AE650676-C6E6-4D6F-848F-55747F42E172")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("GroupB_Project")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GroupB_Project")]
