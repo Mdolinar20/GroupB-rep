c7199fee24eec484e1dc82f70dd464066a2a001e
JaeWook Jung <jjw2011@icloud.com>
2021-12-04 11:22:17 -0800

    Google Canlender API

---
+++ GroupB_Project/Models/CalenderEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupB_Project.Models
+{
+    public class CalenderEvent : IEnumerable
+    {
+        public string Summery { get; set; }
+        public string Organizer { get; set; }
+        public string Description { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
